b8917370f0500c1e0a28d5e517721470de8266e5
rlewancz <rlewancz@mion.elka.pw.edu.pl>
2020-01-17 11:57:43 +0100

    added nodes with percentages

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ y = np.array(["(30k,38k)",
               "(23k,30k)"])
 
 Z = np.array([["rainy", "hot", "high", "false"],
+              ["rainy", "hot", "high", "false"],
                ["rainy", "hot", "high", "true"],
               ["overcast", "hot", "high", "false"],
               ["sunny", "mild", "high", "false"],
@@ -56,7 +57,7 @@ Z = np.array([["rainy", "hot", "high", "false"],
               ["overcast", "mild", "high", "true"],
               ["overcast", "hot", "normal", "false"],
               ["sunny", "mild", "high", "true"]])
-t = np.array(["no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes", "yes", "yes", "no"])
+t = np.array(["no", "yes", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes", "yes", "yes", "no"])
 data = DataFrame(Z,columns=["Outlook", "Temp", "Humidity", "Windy"])
 data['class'] = t
 # data = DataFrame(X, columns = ["age", "gender", "sector", "degree"])
